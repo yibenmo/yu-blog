f405cc6cfd45c16d5d9cea0bac5fc6625b614843
yibenmo <1457124401@qq.com>
2022-03-17 18:42:33 +0800

    create a blog

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import elementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(elementUI)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/view/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-header :height='200'>
+      <el-col><el-avatar :size="'large'" :src="scr"></el-avatar></el-col>
+      <el-col><h1>YuAo</h1></el-col>
+      
+    </el-header>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
